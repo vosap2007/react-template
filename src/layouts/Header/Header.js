eb0a648049dc57ccac7871c0331a0f3572fe272a
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2021-12-23 13:18:32 +0200

    добавляет Header

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,9 +1,22 @@
+/* eslint-disable */
 import React from 'react';
-const Header = () => (
-  <>
-    <h1>Header</h1>
-    <button type="button">Hello!</button>
-  </>
-);
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+          </Nav>
+          <Button variant="warning">Connect to Wallet</Button>{' '}
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
 export default Header;
